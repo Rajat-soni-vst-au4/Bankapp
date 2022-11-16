84e2756b3b98a6adf9816eaaf655ead5fa11cef7
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-11-17 00:51:55 +0530

    folder structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Bussiness,
+  Clients,
+  CTA,
+  Stats,
+  Footar,
+  Testimonials,
+  Hero
+} from "./Components";
 
-function App() {
+const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={"${styles.paddingsX} ${styles.flexCenter}"}>
+        <div className={"${styles.boxwidth}"}>
+          <Navbar />
+        </div>
+      </div>
+      <div className={"bg-primary ${styles.flexStart}"}>
+        <div className={"${styles.boxWidth}"}>
+          <Hero />
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Bussiness />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footar />
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Footar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footar() {
+  return (
+    <div>Footar</div>
+  )
+}
+
+export default Footar
