e6a4d53f6dc69dc629bfb25b5ed0a16233b77316
Rajat-soni-vst-au4 <Rajatsoni.rs66@gmail.com>
2022-11-18 23:59:18 +0530

    discount text added

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,6 +7,14 @@ function Hero() {
       <div className={'flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6'}>
         <div className={'flex flex-row items-center pv-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2'}>
           <img src={discount} alt="discount" className='w-[32px] h-[32px]'/>
+          <p>
+          <span className='text-white'>20%</span>
+            Discount for {" "}
+          <span className='text-white'>1 Month</span>
+            Account
+
+          </p>
+         
         </div>
       </div>
 
